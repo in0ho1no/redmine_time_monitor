be761a3b4c837979f5d0a8591d01fbdf4947cf3b
in0ho1no <yin0ho1no@gmail.com>
2025-12-06 00:13:43 +0900

    設定ファイル更新

--- src/user_setting.py
+++ src/user_setting.py
@@ -1,8 +1,13 @@
-REDMINE_URL = 'https://your-redmine-url.com'  # あなたのRedmineのURLに変更
-API_KEY = 'YOUR_API_KEY'  # 手順1で取得したAPIキー
+REDMINE_URL = 'https://your-redmine-url.com'  # RedmineのURL
+REDMINE_API_KEY = 'YOUR_API_KEY'  # RedmineのAPIキー
 
 # 対象プロジェクトの識別子 (URLの projects/ の後ろにある文字列)
 # 例: https://.../projects/system_dev/settings -> 'system_dev'
 TARGET_PROJECT_ID = 'projects'
 
+# 新規チケット作成する際のトラッカー指定
 TRACKER_ID = 3
+
+# requestのオプション
+HEADERS = {'X-Redmine-API-Key': REDMINE_API_KEY}
+REQUEST_OPTS = {'headers': HEADERS, 'verify': False}
